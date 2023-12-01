87a2acdacb35ea966e794d91f283b558ec71a4a3
Elias Leon <elias.leonhardsberger@gmail.com>
2023-12-01 16:48:18 +0100

    Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2023.Day1;
+
+public partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var sum1 = 0;
+        var sum2 = 0;
+        using (var file = File.OpenText("D:\\AoC\\InputFiles\\Day1.txt"))
+        {
+            while (!file.EndOfStream)
+            {
+                var input = await file.ReadLineAsync()
+                    ?? throw new Exception("No string found");
+                input = input.ToLower();
+                sum1 += GetEncodedNumber(input);
+                var task2 = ConvertNumbers(input);
+                sum2 += GetEncodedNumber(task2);
+            }
+        }
+        Console.WriteLine("Task 1:");
+        Console.WriteLine(sum1);
+        Console.WriteLine("Task 2:");
+        Console.WriteLine(sum2);
+    }
+
+    private static int GetEncodedNumber(string input)
+    {
+        var matches = DigitRegex().Matches(input);
+        if (matches.Count == 0)
+        {
+            throw new Exception("No digit in line found");
+        }
+        var orderedMatches = matches.OrderBy(m => m.Index);
+        var firstDigit = int.Parse(orderedMatches.First().Value);
+        var secondDigit = int.Parse(orderedMatches.Last().Value);
+        return firstDigit * 10 + secondDigit;
+    }
+
+    private static string ConvertNumbers(string input) =>
+        input.Replace("one", "one1one")
+            .Replace("two", "two2two")
+            .Replace("three", "three3three")
+            .Replace("four", "four4four")
+            .Replace("five", "five5five")
+            .Replace("six", "six6six")
+            .Replace("seven", "seven7seven")
+            .Replace("eight", "eight8eight")
+            .Replace("nine", "nine9nine");
+
+    [GeneratedRegex("[0-9]")]
+    private static partial Regex DigitRegex();
+}
