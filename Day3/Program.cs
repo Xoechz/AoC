4a24977ce0e26110f666c24adbb8678e59e39979
Elias Leon <elias.leonhardsberger@gmail.com>
2023-12-04 19:57:20 +0100

    added day3 and 4

---
+++ Day3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2023.Day3;
+
+public partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var sum1 = 0;
+        var sum2 = 0;
+        List<string> schematic = [];
+        using (var file = File.OpenText("D:\\AoC2023\\InputFiles\\Day3.txt"))
+        {
+            while (!file.EndOfStream)
+            {
+                var inputString = await file.ReadLineAsync()
+                    ?? throw new Exception("No string found");
+                schematic.Add(inputString);
+            }
+        }
+
+        //No number has 2 symbols as neighbors
+        for (int i = 0; i < schematic.Count; i++)
+        {
+            var matches = Symbol().Matches(schematic[i]);
+            foreach (var match in matches.ToList())
+            {
+                List<int> numbers = [];
+                var lineLenght = schematic[i].Length;
+                var symbolIndex = match.Index;
+
+                if (i != 0)
+                {
+                    numbers.AddRange(Number().Matches(schematic[i - 1])
+                        .Where(n => symbolIndex > n.Index - 2
+                            && symbolIndex < n.Index + n.Length + 1)
+                        .Select(n => int.Parse(n.Value)));
+                }
+
+                numbers.AddRange(Number().Matches(schematic[i])
+                    .Where(n => symbolIndex > n.Index - 2
+                        && symbolIndex < n.Index + n.Length + 1)
+                    .Select(n => int.Parse(n.Value)));
+
+                if (i != schematic.Count - 1)
+                {
+                    numbers.AddRange(Number().Matches(schematic[i + 1])
+                        .Where(n => symbolIndex > n.Index - 2
+                            && symbolIndex < n.Index + n.Length + 1)
+                        .Select(n => int.Parse(n.Value)));
+                }
+
+                sum1 += numbers.Sum();
+                if (match.Value == "*" && numbers.Count == 2)
+                {
+                    sum2 += numbers[0] * numbers[1];
+                }
+            }
+        }
+
+        Console.WriteLine("Task 1:");
+        Console.WriteLine(sum1);
+        Console.WriteLine("Task 2:");
+        Console.WriteLine(sum2);
+    }
+
+    [GeneratedRegex("[0-9]+")]
+    private static partial Regex Number();
+
+    [GeneratedRegex("[^0-9.]")]
+    private static partial Regex Symbol();
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace AoC2023.Day4;
+
+public partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var sum1 = 0;
+        var sum2 = 0;
+        var card = 0;
+        var duplicateDict = new Dictionary<int, int>();
+        using (var file = File.OpenText("D:\\AoC2023\\InputFiles\\Day4.txt"))
+        {
+            while (!file.EndOfStream)
+            {
+                var inputString = await file.ReadLineAsync()
+                    ?? throw new Exception("No string found");
+                var parts = inputString.Split('|');
+                if (parts.Length != 2)
+                {
+                    throw new Exception("Invalid Card Format.");
+                }
+                var yourNumbers = parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var winningNumbers = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var scoringNumbers = yourNumbers.Where(n => winningNumbers.Contains(n))
+                    .Select(int.Parse)
+                    .ToList();
+
+                if (scoringNumbers.Count != 0)
+                {
+                    sum1 += (int)Math.Pow(2, scoringNumbers.Count - 1);
+                }
+
+                var cardCount = duplicateDict.TryGetValue(card, out var duplication) ? duplication + 1 : 1;
+                sum2 += cardCount;
+
+                for (var i = 1; i <= scoringNumbers.Count; i++)
+                {
+                    if (duplicateDict.ContainsKey(card + i))
+                    {
+                        duplicateDict[card + i] += cardCount;
+                    }
+                    else
+                    {
+                        duplicateDict[card + i] = cardCount;
+                    }
+                }
+
+                card++;
+            }
+        }
+        Console.WriteLine("Task 1:");
+        Console.WriteLine(sum1);
+        Console.WriteLine("Task 2:");
+        Console.WriteLine(sum2);
+    }
+}
