ba8e5ceb5685d53e0ccf0d077be279aeaa8048f8
Elias Leon <elias.leonhardsberger@gmail.com>
2023-12-12 19:41:00 +0100

    added Day12

---
+++ Day12/Models/Spring.cs
@@ -0,0 +1,107 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2023.Day12.Models;
+
+public partial class Spring(string id, List<int> counts, long value = 1)
+{
+    #region Public Properties
+
+    public List<int> Counts { get; set; } = [.. counts];
+    public string Id { get; set; } = id;
+    public long Value { get; set; } = value;
+
+    #endregion Public Properties
+
+    #region Public Methods
+
+    public bool IsValid()
+    {
+        var countsCopy = Counts.ToList();
+        var count = 0;
+        foreach (var c in Id)
+        {
+            if (c == '?')
+            {
+                if (countsCopy.Count == 0)
+                {
+                    return true;
+                }
+                if (countsCopy[0] >= count)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (c == '#')
+            {
+                count++;
+                if (countsCopy.Count == 0)
+                {
+                    return false;
+                }
+            }
+            else if (count > 0)
+            {
+                if (countsCopy[0] == count)
+                {
+                    countsCopy.RemoveAt(0);
+                    count = 0;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return countsCopy.Count == 0
+            || (countsCopy.Count == 1
+                && countsCopy[0] == count);
+    }
+
+    public void Shorten()
+    {
+        var count = 0;
+        var index = 0;
+        var removeTo = 0;
+        foreach (var c in Id)
+        {
+            if (c == '?')
+            {
+                break;
+            }
+            else if (c == '#')
+            {
+                count++;
+            }
+            else if (count > 0)
+            {
+                if (Counts[0] == count)
+                {
+                    Counts.RemoveAt(0);
+                    removeTo = index;
+                    count = 0;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            index++;
+        }
+        var regex = MultiplePoints();
+        Id = regex.Replace(Id[removeTo..], ".").Trim('.');
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    [GeneratedRegex("\\.+")]
+    private static partial Regex MultiplePoints();
+
+    #endregion Private Methods
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,82 @@
+﻿using AoC2023.Day12.Models;
+
+namespace AoC2023.Day12;
+
+public partial class Program
+{
+    #region Public Methods
+
+    public static async Task Main(string[] args)
+    {
+        long sum1 = 0;
+        long sum2 = 0;
+
+        using (var file = File.OpenText("D:\\AoC2023\\InputFiles\\Day12.txt"))
+        {
+            while (!file.EndOfStream)
+            {
+                var inputString = await file.ReadLineAsync()
+                     ?? throw new Exception("No string found");
+                var parts = inputString.Split(' ');
+                var spring1 = new Spring(parts[0],
+                    parts[1].Split(',')
+                        .Select(int.Parse)
+                        .ToList());
+
+                var spring2String = spring1.Id + "?" + spring1.Id + "?" + spring1.Id + "?" + spring1.Id + "?" + spring1.Id;
+                var spring2Counts = spring1.Counts.ToList();
+                spring2Counts.AddRange(spring1.Counts);
+                spring2Counts.AddRange(spring1.Counts);
+                spring2Counts.AddRange(spring1.Counts);
+                spring2Counts.AddRange(spring1.Counts);
+
+                var spring2 = new Spring(spring2String, spring2Counts);
+                sum1 += GetNumberOfAllPossibleOptions(spring1);
+                sum2 += GetNumberOfAllPossibleOptions(spring2);
+            }
+        }
+
+        Console.WriteLine("Task 1:");
+        Console.WriteLine(sum1);
+        Console.WriteLine("Task 2:");
+        Console.WriteLine(sum2);
+    }
+
+    private static long GetNumberOfAllPossibleOptions(Spring spring)
+    {
+        List<Spring> options = [spring];
+
+        while (options.Any(d => d.Id.Contains('?')))
+        {
+            List<Spring> next = options.Where(d => !d.Id.Contains('?'))
+                .ToList();
+            foreach (var option in options.Where(d => d.Id.Contains('?')))
+            {
+                var currentString = option.Id;
+                var index = currentString.IndexOf('?');
+                var nextSpring1 = new Spring(currentString.Remove(index, 1).Insert(index, "."), option.Counts, option.Value);
+                var nextSpring2 = new Spring(currentString.Remove(index, 1).Insert(index, "#"), option.Counts, option.Value);
+                if (nextSpring1.IsValid())
+                {
+                    nextSpring1.Shorten();
+                    next.Add(nextSpring1);
+                }
+                if (nextSpring2.IsValid())
+                {
+                    nextSpring2.Shorten();
+                    next.Add(nextSpring2);
+                }
+            }
+
+            options = next
+                .GroupBy(g => g.Id + " " + g.Counts.Count)
+                .Select(g => new Spring(g.First().Id, g.First().Counts, g.Sum(s => s.Value)))
+                .ToList();
+        }
+
+        return options.Where(s => s.IsValid())
+            .Sum(s => s.Value);
+    }
+
+    #endregion Public Methods
+}
